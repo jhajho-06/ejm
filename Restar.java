132d374810b64e380d83ae2948aaafa03a1bc1b0
jhajho-06 <agilajhoel06@gmail.com>
2026-04-12 18:28:28 -0500

    Nueva funcion

---
+++ Restar.java
@@ -0,0 +1,8 @@
+public class Restar {
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 5;
+            int resultado = a - b;
+            System.out.println("El resultado de la resta es: " + resultado);
+        }
+}
